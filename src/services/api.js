250a9cd618517dcfaee39670acd54ba458a93c68
Lucas Conde <lucascondeferreira@gmail.com>
2024-12-30 23:55:22 -0300

    -v0.1.2 | Conectando Api na Nuvem

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-const Api = axios.create({ baseURL: 'http://localhost:3001' });
+const Api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001',
+});
 
 export default Api;
